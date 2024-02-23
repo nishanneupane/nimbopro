dbfae85cee539f993ea09fee3be85a04f68a8a92
unknown <nishanneupane368@gmail.com>
2024-02-23 19:49:08 +0545

    update : subscription ui

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
@@ -11,9 +11,9 @@ interface SubscriptionButtonProps {
   isPro: boolean;
 };
 
-export const SubscriptionButton = ({ 
+export const SubscriptionButton = ({
   isPro,
- }: SubscriptionButtonProps) => {
+}: SubscriptionButtonProps) => {
   const proModal = useProModal();
 
   const { execute, isLoading } = useAction(stripeRedirect, {
@@ -34,12 +34,17 @@ export const SubscriptionButton = ({
   }
 
   return (
-    <Button
-      variant="primary"
-      onClick={onClick}
-      disabled={isLoading}
-    >
-      {isPro ? "Manage subscription" : "Upgrade to pro"}
-    </Button>
+    <div className="flex flex-col gap-3 items-start justify-start">
+      <div className="flex items-center justify-center">
+        <h1 className="bg-gradient-to-r from-blue-500 to-teal-400 text-transparent bg-clip-text p-2 rounded-md mt-3 font-3xl md:font-6xl font-extrabold">You are on {isPro ? "pro " : "free "} plan</h1>
+      </div>
+      <Button
+        variant="default"
+        onClick={onClick}
+        disabled={isLoading}
+      >
+        {isPro ? "Manage subscription" : "Upgrade to pro"}
+      </Button>
+    </div>
   )
 };
