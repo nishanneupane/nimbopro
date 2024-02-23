69a1450df9ab0139347870d17ce3acf9f14970c2
unknown <nishanneupane368@gmail.com>
2024-02-23 19:50:59 +0545

    update : subscription ui

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -66,7 +66,7 @@ const MarketingPage = () => {
             {/* Services Section */}
             <section className="py-10 w-full">
                 <div className="max-w-6xl mx-auto px-4">
-                    <h2 className="text-3xl font-bold text-gray-800 mb-6">What you'll get?</h2>
+                    <h2 className="text-3xl font-bold text-gray-800 mb-6">What you&apos;ll get?</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         <div className="bg-white shadow-md rounded-lg p-6">
                             <h3 className="text-xl font-extrabold bg-gradient-to-r from-blue-500 to-teal-400 text-white mb-2 w-[50px] p-2 rounded-md"><TabletSmartphone className='h-8 w-8 mr-2' /></h3>
