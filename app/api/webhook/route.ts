6f1ced69f2fc138a2645a4d8e4dad3e529763b59
unknown <nishanneupane368@gmail.com>
2024-02-24 18:07:15 +0545

    update : something

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -18,52 +18,51 @@ export async function POST(req: Request) {
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
-
-    const session = event.data.object as Stripe.Checkout.Session;
-
-    if (event.type === "checkout.session.completed") {
-      const subscription = await stripe.subscriptions.retrieve(
-        session.subscription as string
-      );
-
-      if (!session?.metadata?.orgId) {
-        return new NextResponse("Org ID is required", { status: 400 });
-      }
-
-      await db.orgSubscription.create({
-        data: {
-          orgId: session?.metadata?.orgId,
-          stripeSubscriptionId: subscription.id,
-          stripeCustomerId: subscription.customer as string,
-          stripePriceId: subscription.items.data[0].price.id,
-          stripeCurrentPeriodEnd: new Date(
-            subscription.current_period_end * 1000
-          ),
-        },
-      });
-    }
-
-    if (event.type === "invoice.payment_succeeded") {
-      const subscription = await stripe.subscriptions.retrieve(
-        session.subscription as string
-      );
-
-      await db.orgSubscription.update({
-        where: {
-          stripeSubscriptionId: subscription.id,
-        },
-        data: {
-          stripePriceId: subscription.items.data[0].price.id,
-          stripeCurrentPeriodEnd: new Date(
-            subscription.current_period_end * 1000
-          ),
-        },
-      });
-    }
-
-    return new NextResponse(null, { status: 200 });
   } catch (error) {
-    console.log("WEBHOOK_ERROR", error);
     return new NextResponse("Webhook error", { status: 400 });
   }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    if (!session?.metadata?.orgId) {
+      return new NextResponse("Org ID is required", { status: 400 });
+    }
+
+    await db.orgSubscription.create({
+      data: {
+        orgId: session?.metadata?.orgId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    await db.orgSubscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
 }
